0ba11aad2f429de99d10a608d7faad6fab0f61e8
style7en <zhangjia_86@126.com>
2014-06-29 21:53:34 +0800

    增加Test工程

---
+++ c/QT_test/QT_test/test.cpp
@@ -0,0 +1,7 @@
+#include <QtGui>
+#include "test.h"
+
+Test::Test(QWidget *parent/* =0 */) : QWidget(parent)
+{
+    
+}
---
+++ c/QT_test/QT_test/test.h
@@ -0,0 +1,16 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include <QWidget>
+
+class Test : public QWidget
+{
+    Q_OBJECT
+public:
+    Test(QWidget *parent=0);
+signals:
+private slots:
+private:
+};
+
+#endif
